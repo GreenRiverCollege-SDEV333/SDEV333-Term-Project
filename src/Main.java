f5e8a473a53cf72867f4f197de2c905692476212
Gecervantes01 <cervantes.giovan@student.greenriver.edu>
2024-02-07 11:42:05 -0800

    Added test in main for ArrayList

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,12 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
 
+        ArrayList<Integer> listTest = new ArrayList<>();
+        listTest.addFront(21);
+        for(Integer i : listTest) {
+            System.out.println(i);
+        }
+
+
     }
 }
