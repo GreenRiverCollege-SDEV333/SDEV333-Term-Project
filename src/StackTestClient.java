7b806115a9fedcb8fbfa4cdcd610142934433b70
Clav <smith.rob@student.greenriver.edu>
2024-02-07 17:53:08 -0800

    Validated and working ResizingArrayStack and passes StackTestClient to match page 137 in Algorithms.

--- src/StackTestClient.java
+++ src/StackTestClient.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 
 /**
- * StackTestClient for term-project assignment 2 to implement and test a Stack ADT.
+ * StackTestClient for term-project assignment 2 to implement and test a Stack ADT class, ResizingArrayStack..
  * @author Rob Smith
  * @version 1.0
  */
 public class StackTestClient {
     public static void main(String[]args){
 
+        Stack<String> s = new ResizingArrayStack<String>();
+        Scanner in = new Scanner ("to be or not to - be - - that - - - is");
 
-
-
-
-
+        while (in.hasNext()) {
+            String item = in.next();
+            if (!item.equals("-")){
+                s.push(item);
+            } else if (!s.isEmpty()) {
+                System.out.println(s.pop()+" ");
+            }
+        }
+        System.out.println("(" + s.size() + " left on the stack"+")");
     }
 }
