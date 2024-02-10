31d012f643a4f575a9dc9cf0636552b741a24bde
Lois Lanctot <lanctot.lois@student.greenriver.edu>
2024-02-10 15:57:53 -0800

    Created ArrayList

---
+++ src/ArrayList.java
@@ -0,0 +1,292 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ArrayList<E> implements List<E> {
+
+    private int size;
+    private E[] buffer;
+
+    /**
+     * Constructor for an ArrayIntList object
+     */
+    public ArrayList() {
+        size = 0;
+        this.buffer = (E[]) new Object[10];
+    }
+
+
+    /**
+     * Add item to the front.
+     *
+     * @param item the item to be added
+     */
+    @Override
+    public void addFront(E item) {
+        if (size == buffer.length) {
+            resize(size * 2);
+        }
+
+        for (int i = size; i >= 1; i--) {
+            buffer[i] = buffer[i - 1];
+        }
+
+        buffer[0] = item;
+        size++;
+    }
+
+    /**
+     * Add item to the back.
+     *
+     * @param item the item to be added
+     */
+    @Override
+    public void addBack(E item) {
+        if (size == buffer.length) {
+            resize(size * 2);
+        }
+        buffer[size] = item;
+        size++;
+    }
+
+    /**
+     * Add an item at specified index (position).
+     *
+     * @param i    the index where the item should be added
+     * @param item the item to be added
+     */
+    @Override
+    public void add(int i, E item) {
+        if (i == 0 && size == 0) {
+            size++;
+        }
+        else if (i < 0 || i > size) {
+            throw new IndexOutOfBoundsException("index is out of bounds");
+        }
+        if (size == buffer.length) {
+            resize(size * 2);
+        }
+
+        for (int j = size; j >= i; j--) {
+            buffer[j] = buffer[j - 1];
+        }
+
+        buffer[i] = item;
+        size++;
+    }
+
+    /**
+     * Get the item at a specified index.
+     *
+     * @param i the index where the item should be retrieved
+     * @return the item located at that index
+     */
+    @Override
+    public E get(int i) {
+        if (i >= size) {
+            throw new IndexOutOfBoundsException("Index cannot be greater than size");
+        }
+        else if (i < 0) {
+            throw new IndexOutOfBoundsException("Index cannot be less than zero");
+        }
+        return buffer[i];
+    }
+
+    /**
+     * Set (save) an item at a specified index. Previous
+     * item at that index is overwritten.
+     *
+     * @param i    the index where the item should be saved
+     * @param item the item to be saved
+     */
+    @Override
+    public void set(int i, E item) {
+        //if index is out of bounds
+        if (i < 0 || i > size) {
+            throw new IndexOutOfBoundsException("index is out of bounds");
+        }
+
+        buffer[i] = item;
+    }
+
+    /**
+     * Remove item at the front of the list.
+     *
+     * @return the item that was removed
+     */
+    @Override
+    public E removeFront() {
+        if (size != 0) {
+
+            E frontItem = null;
+            for (int i = 0; i <= size; i++) {
+                frontItem = buffer[i];
+                buffer[i] = buffer[i+1];
+            }
+            size--;
+            return frontItem;
+        }
+        return null;
+    }
+
+    /**
+     * Remove item at the back of the list
+     *
+     * @return the item that was removed
+     */
+    @Override
+    public E removeBack() {
+        if (size != 0) {
+            E frontItem = buffer[size];
+            buffer[size] = null;
+            size--;
+            return frontItem;
+        }
+        return null;
+    }
+
+    /**
+     * Remove item from the list
+     *
+     * @param item the item to be removed
+     */
+    @Override
+    public void remove(E item) {
+        for (int i = 0; i < size; i++) {
+            if (buffer[i] == item) {
+                buffer[i] = buffer[i+1];
+                size--;
+            }
+        }
+
+    }
+
+    /**
+     * Remove item at a specified index.
+     *
+     * @param i the index where the item should be removed
+     * @return the item that was removed
+     */
+    @Override
+    public E remove(int i) {
+        //validate index
+        if (i >= size) {
+            throw new IndexOutOfBoundsException("Index cannot be greater than size");
+        }
+        else if (i < 0) {
+            throw new IndexOutOfBoundsException("Index cannot be less than zero");
+        }
+
+        //save a copy of the value to be removed, so we can return it latter
+        E copyOfRemovedValue = buffer[i];
+
+        //shift values to the left
+        for (int j = i; j <= size - 1; j++) {
+            buffer[j] = buffer[j + 1];
+        }
+
+        buffer[size - 1] = null;
+
+        //don't forget to decrease size
+        size--;
+
+        return copyOfRemovedValue;
+    }
+
+    /**
+     * Checks if an item is in the list.
+     *
+     * @param item the item to search for
+     * @return true if the item is in the list, false otherwise
+     */
+    @Override
+    public boolean contains(E item) {
+        for (int i = 0; i < size; i++) {
+            if (buffer[i] == item) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the list is empty.
+     *
+     * @return true if the list is empty, false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Provides a count of the number of items in the list.
+     *
+     * @return number of items in the list
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+
+    private void resize(int newSize) {
+        //create new space, separate from the old space
+        E[] newBuffer = (E[]) new Object[newSize];
+
+        //copy everything over from buffer into newBuffer
+        for (int i = 0; i < buffer.length; i++) {
+            newBuffer[i] = buffer[i];
+        }
+        buffer = newBuffer;
+
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code E}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new ArrayListIterator();
+    }
+
+    private class ArrayListIterator implements Iterator<E> {
+        //private fields
+        private int i;
+
+        private ArrayListIterator() {
+            i = 0;
+        }
+
+        /**
+         * Returns true if the iteration has more elements.
+         *
+         * @return true if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return i < size;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public E next() {
+            if (i >= size) {
+                throw new NoSuchElementException("i is now out of bounds");
+            }
+
+            E currentValue = buffer[i];
+            i++;
+
+            return currentValue;
+        }
+    }
+
+
+}
