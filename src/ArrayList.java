7058215c0664b007e227adcf435eacb9439cc0b0
Sage Bain <bain.sage@student.greenriver.edu>
2024-02-10 16:22:37 -0800

    Implemented Method Headers/

---
+++ src/ArrayList.java
@@ -0,0 +1,66 @@
+public class ArrayList implements List<E>{
+    @java.lang.Override
+    public void addFront(E item) {
+
+    }
+
+    @java.lang.Override
+    public void addBack(E item) {
+
+    }
+
+    @java.lang.Override
+    public void add(int i, E item) {
+
+    }
+
+    @java.lang.Override
+    public E get(int i) {
+        return null;
+    }
+
+    @java.lang.Override
+    public void set(int i, E item) {
+
+    }
+
+    @java.lang.Override
+    public E removeFront() {
+        return null;
+    }
+
+    @java.lang.Override
+    public E removeBack() {
+        return null;
+    }
+
+    @java.lang.Override
+    public void remove(E item) {
+
+    }
+
+    @java.lang.Override
+    public E remove(int i) {
+        return null;
+    }
+
+    @java.lang.Override
+    public boolean contains(E item) {
+        return false;
+    }
+
+    @java.lang.Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @java.lang.Override
+    public int size() {
+        return 0;
+    }
+
+    @java.lang.Override
+    public java.util.Iterator<T> iterator() {
+        return null;
+    }
+}
