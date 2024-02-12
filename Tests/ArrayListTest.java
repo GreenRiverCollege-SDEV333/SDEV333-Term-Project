6b4126b70fdf76ec4db4054c1b91747b629aff4c
Leah Konma <leahkonma02@gmail.com>
2024-02-12 14:53:50 -0800

    created test class for ArrayList

---
+++ Tests/ArrayListTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+public class ArrayListTest<T> {
+    ArrayList<T> testList = new ArrayList<>();
+    @Test
+    void addFront(){
+
+    }
+
+    @Test
+    void addBack(){
+
+    }
+    @Test
+    void add(){
+
+    }
+    @Test
+    void get(){
+
+    }
+
+}
