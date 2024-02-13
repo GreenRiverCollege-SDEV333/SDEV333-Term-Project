db4c0d96915db7bcfc491b15f4a73b21b3da583c
Sage Bain <bain.sage@student.greenriver.edu>
2024-02-12 18:02:52 -0800

    Completed all add methods.

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -18,17 +18,99 @@ public class LinkedList<E> implements List<E>{
     }
     @Override
     public void addFront(E item) {
-
+        Node newOne = new Node();
+        newOne.data = item;
+        if(head == null)
+        {
+            // the list is currently empty
+            newOne.next = null;
+            head = newOne;
+            size++;
+            return;
+        }
+        // the list currently has some nodes in it
+        newOne.next = head;
+        head = newOne;
+        size++;
     }
 
     @Override
     public void addBack(E item) {
-
+        Node newOne = new Node();
+        newOne.data = item;
+        if(head == null)
+        {
+            // the list is currently empty
+            newOne.next = null;
+            head = newOne;
+            size++;
+            return;
+        }
+        // the list currently has some nodes in it
+        Node current = head;
+        while(current.next != null)
+        {
+            current = current.next;
+        }
+        current.next = newOne;
+        size++;
     }
 
     @Override
     public void add(int i, E item) {
-
+// throw exception if index given is out of bounds
+        if(i > size || i < 0)
+        {
+            throw new IndexOutOfBoundsException("Invalid index given!");
+        }
+        if(head == null)
+        {
+            head = new Node();
+            head.data = item;
+            head.next = null;
+            size++;
+            return;
+        }
+        // current Node to keep track of index
+        Node current = head;
+        // create a new Node
+        Node theNewOne = new Node();
+        // set the new Nodes value
+        theNewOne.data = item;
+        // check if index is at the front of the LinkedList
+        if(i == 0)
+        {
+            theNewOne.next = head;
+            head = theNewOne;
+            size++;
+            return;
+        }
+        // check if index is at the end of the LinkedList
+        if(i == size())
+        {
+            // loop to end of the list
+            while(current.next != null)
+            {
+                current = current.next;
+            }
+            // add the new Node
+            current.next = theNewOne;
+            size++;
+            return;
+        }
+        // else, for loop that stops one before new indexed Node
+        for (int j = 0; j < i-1; j++) {
+            current = current.next;
+        }
+        // save the old Node from that index so we can move it forward one space
+        Node nodeAfterAdded = current.next;
+        // put the new Node in current.next position
+        current.next = theNewOne;
+        // move current forward one
+        current = current.next;
+        // set the previous index Node forward one index position (e.g. index 3 Node would now become index 4)
+        current.next = nodeAfterAdded;
+        size++;
     }
 
     @Override
