6320b7cfa6969cedae113f926113eb37749435aa
theZalmanian <thezalmanian@gmail.com>
2024-02-13 04:23:01 -0800

    Created test directory and setup test file for ArrayList

---
+++ tests/ArrayListTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+
+class ArrayListTest {
+    /**
+     * Create ArrayList at start for testing
+     */
+    private final ArrayList<Integer> testArrayList = new ArrayList<>();
+
+    @Test
+    void addFront() {
+    }
+
+    @Test
+    void addBack() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void removeFront() {
+    }
+
+    @Test
+    void removeBack() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void size() {
+    }
+}
