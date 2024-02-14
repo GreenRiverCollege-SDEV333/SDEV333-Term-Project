e506d922e3ac55b3f807bb01a9b00bd97a5d7a72
theZalmanian <thezalmanian@gmail.com>
2024-02-14 07:21:47 -0800

    Set up LinkedList class and test files

---
+++ tests/LinkedListTest.java
@@ -0,0 +1,78 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+    /**
+     * Create LinkedList at start for testing
+     */
+    private final LinkedList<Integer> testLinkedList = new LinkedList<>();
+
+    /**
+     * The first index in list is 0
+     */
+    private final int FIRST_INDEX = 0;
+
+    /**
+     * The item returned by methods if item not found
+     */
+    private final int INVALID_INDEX = -1;
+
+    /**
+     * The standard filler item added to container prior to testing method
+     */
+    private final int FILLER_ITEM = 5;
+
+    /**
+     * The standard item used to test methods
+     */
+    private final int TEST_ITEM = 20;
+
+    @Test
+    void addFront() {
+    }
+
+    @Test
+    void addBack() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void removeFront() {
+    }
+
+    @Test
+    void removeBack() {
+    }
+
+    @Test
+    void removeItem() {
+    }
+
+    @Test
+    void removeIndex() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void size() {
+    }
+}
