b5b6c20c19575ce47edcb1c6484977ee141d46ea
InfiniteGmng <noah.lanctot@gmail.com>
2024-02-13 20:47:15 -0800

    Added documentation and runtime definitions for each method

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,11 +1,21 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Implementation of a singly linked list.
+ *
+ * @author Noah Lanctot
+ * @version 1.0
+ * @param <E> the type of elements in this list
+ */
 public class LinkedList<E> implements List<E> {
 
     private Node<E> head;
     private int size;
 
+    /**
+     * Constructs an empty LinkedList.
+     */
     public LinkedList() {
         head = null;
         size = 0;
@@ -21,6 +31,12 @@ public class LinkedList<E> implements List<E> {
         }
     }
 
+    /**
+     * Adds the specified element to the front of this list.
+     * This method has a runtime complexity of O(1).
+     *
+     * @param item the element to be added to the front of this list
+     */
     @Override
     public void addFront(E item) {
         Node<E> newNode = new Node<>(item);
@@ -29,6 +45,14 @@ public class LinkedList<E> implements List<E> {
         size++;
     }
 
+    /**
+     * Adds the specified element to the back of this list.
+     * This method has a runtime complexity of O(n),
+     * where n is the number of elements in the list, because it
+     * may need to traverse the list to find the last node.
+     *
+     * @param item the element to be added to the back of this list
+     */
     @Override
     public void addBack(E item) {
         if (head == null) {
@@ -44,6 +68,16 @@ public class LinkedList<E> implements List<E> {
         }
     }
 
+    /**
+     * Adds the specified element at the specified index in this list.
+     * This method has a runtime complexity of O(n),
+     * where n is the number of elements in the list, because it
+     * may need to traverse the list to find the node before the specified index.
+     *
+     * @param index the index at which the specified element is to be inserted
+     * @param item the element to be inserted
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
     @Override
     public void add(int index, E item) {
         if (index < 0 || index > size) {
@@ -63,6 +97,16 @@ public class LinkedList<E> implements List<E> {
         }
     }
 
+    /**
+     * Returns the element at the specified index in this list.
+     * This method has a runtime complexity of O(n),
+     * where n is the index of the element to be retrieved,
+     * because it may need to traverse the list to find the node at the specified index.
+     *
+     * @param index the index of the element to return
+     * @return the element at the specified index in this list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
     @Override
     public E get(int index) {
         if (index < 0 || index >= size) {
@@ -75,6 +119,16 @@ public class LinkedList<E> implements List<E> {
         return current.data;
     }
 
+    /**
+     * Replaces the element at the specified index in this list with the specified element.
+     * This method has a runtime complexity of O(n),
+     * where n is the index of the element to be replaced,
+     * because it may need to traverse the list to find the node at the specified index.
+     *
+     * @param index the index of the element to replace
+     * @param item the element to be stored at the specified index
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
     @Override
     public void set(int index, E item) {
         if (index < 0 || index >= size) {
@@ -87,6 +141,13 @@ public class LinkedList<E> implements List<E> {
         current.data = item;
     }
 
+    /**
+     * Removes and returns the element at the front of this list.
+     * This method has a runtime complexity of O(1).
+     *
+     * @return the element at the front of this list
+     * @throws NoSuchElementException if this list is empty
+     */
     @Override
     public E removeFront() {
         if (isEmpty()) {
@@ -98,6 +159,15 @@ public class LinkedList<E> implements List<E> {
         return removedItem;
     }
 
+    /**
+     * Removes and returns the element at the back of this list.
+     * This method has a runtime complexity of O(n),
+     * where n is the number of elements in the list, because it
+     * may need to traverse the list to find the second-to-last node.
+     *
+     * @return the element at the back of this list
+     * @throws NoSuchElementException if this list is empty
+     */
     @Override
     public E removeBack() {
         if (isEmpty()) {
@@ -116,6 +186,14 @@ public class LinkedList<E> implements List<E> {
         return removedItem;
     }
 
+    /**
+     * Removes the first occurrence of the specified element from this list, if it is present.
+     * This method has a runtime complexity of O(n),
+     * where n is the number of elements in the list, because it
+     * may need to traverse the list to find the specified element.
+     *
+     * @param item the element to be removed from this list
+     */
     @Override
     public void remove(E item) {
         if (isEmpty()) {
@@ -136,6 +214,16 @@ public class LinkedList<E> implements List<E> {
         }
     }
 
+    /**
+     * Removes and returns the element at the specified index in this list.
+     * This method has a runtime complexity of O(n),
+     * where n is the index of the element to be removed,
+     * because it may need to traverse the list to find the node before the specified index.
+     *
+     * @param index the index of the element to be removed
+     * @return the element previously at the specified position
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
     @Override
     public E remove(int index) {
         if (index < 0 || index >= size) {
@@ -154,6 +242,15 @@ public class LinkedList<E> implements List<E> {
         return removedItem;
     }
 
+    /**
+     * Returns true if this list contains the specified element.
+     * This method has a runtime complexity of O(n),
+     * where n is the number of elements in the list, because it
+     * may need to traverse the list to find the specified element.
+     *
+     * @param item the element to be checked for containment in this list
+     * @return true if this list contains the specified element
+     */
     @Override
     public boolean contains(E item) {
         Node<E> current = head;
@@ -166,16 +263,34 @@ public class LinkedList<E> implements List<E> {
         return false;
     }
 
+    /**
+     * Returns true if this list contains no elements.
+     * This method has a runtime complexity of O(1).
+     *
+     * @return true if this list contains no elements
+     */
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Returns the number of elements in this list.
+     * This method has a runtime complexity of O(1).
+     *
+     * @return the number of elements in this list
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     * Returns an iterator over the elements in this list.
+     * This method has a runtime complexity of O(1).
+     *
+     * @return an iterator over the elements in this list
+     */
     @Override
     public Iterator<E> iterator() {
         return new LinkedListIterator();
