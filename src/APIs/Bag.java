c2c9f81d47b54d8e187b28c64ac78547632f2326
theZalmanian <thezalmanian@gmail.com>
2024-02-15 02:05:13 -0800

    Created Bag interface

--- src/APIs/Bag.java
+++ src/APIs/Bag.java
@@ -1,5 +1,21 @@
 package APIs;
 
-public interface Bag {
+public interface Bag<E> extends Iterable<E> {
+    /**
+     * Adds the given item to the bag
+     * @param item the item to be added
+     */
+    void add(E item);
 
+    /**
+     * Checks whether the bag is empty, and returns true/false accordingly
+     * @return true if the bag is empty; otherwise false
+     */
+    boolean isEmpty();
+
+    /**
+     * Gets and returns the number of items stored in the bag
+     * @return the number of items stored in the bag
+     */
+    int size();
 }
