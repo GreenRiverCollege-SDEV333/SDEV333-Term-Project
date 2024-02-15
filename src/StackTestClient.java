7227526eedf90c452cd119a4252f030cb4b8aab1
AddisonFarley <93640684+AddisonFarley@users.noreply.github.com>
2024-02-15 12:42:41 -0800

    Create bag StackTestClient.java

---
+++ src/StackTestClient.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StackTestClient
+{
+    public static void main(String[] args)
+    {
+        Stack<String> s = new ResizingArrayStack<String>;
+
+        Scanner in = new Scanner("to be or not to - be - - that - - - is");
+
+        while(in.hasNext())
+        {
+            String item = in.next();
+
+            if(item.equals("-"))
+            {
+                s.push(item);
+            }
+            else if(!s.isEmpty())
+            {
+                System.out.println("s.pop()" + " ");
+            }
+        }
+
+        System.out.println("(" + s.size() + " left on the stack");
+    }
+}
