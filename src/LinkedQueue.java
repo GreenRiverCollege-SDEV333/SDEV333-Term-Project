fc13f2ffaea1b153a8fdf73ea5d6d2be02e9f128
braedonbillingsley <billingsley.braedon@student.greenriver.edu>
2024-02-16 12:34:23 -0800

    Finished implementation and testing of 2C LinkedQueue and QueueTestClient. Output matching.

---
+++ src/LinkedQueue.java
@@ -0,0 +1,96 @@
+import java.util.Iterator;
+
+public class LinkedQueue<E> implements Queue<E> {
+
+    private Node first; // link to least recently added node
+    private Node last; // link to most recently added node
+    private int size; // number of items on the queue
+
+    public LinkedQueue() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+    private class Node { // nested class to define nodes
+        E item;
+        Node next;
+    }
+
+    /**
+     * Add an item to the queue.
+     *
+     * @param item the item to be added
+     */
+    @Override
+    public void enqueue(E item) {
+        Node oldLast = last; // Add item to the end of the list.
+
+        last = new Node();
+        last.item = item;
+        last.next = null;
+
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldLast.next = last;
+        }
+        size++;
+    }
+
+    /**
+     * Remove an item from the queue.
+     *
+     * @return the item that was removed
+     */
+    @Override
+    public E dequeue() {
+        E item = first.item; // Remove item from the beginning of the list.
+        first = first.next;
+        if (isEmpty()) {
+            last = null;
+        }
+        size--;
+        return item;
+    }
+
+    /**
+     * Checks to see if the queue is empty.
+     *
+     * @return true if the queue is empty, false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return first == null; // Or: N == 0.
+    }
+
+    /**
+     * Returns a count of the number of items in the queue.
+     *
+     * @return the number of items in the queue
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    public Iterator<E> iterator() {
+        return new ListIterator();
+    }
+    private class ListIterator implements Iterator<E> {
+        private Node current = first;
+        public boolean hasNext() {
+            return current != null;
+        }
+        public E next() {
+            E item = current.item;
+            current = current.next;
+            return item;
+        }
+        public void remove() {}
+    }
+}
---
+++ src/QueueTestClient.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class QueueTestClient {
+    public static void main(String[] args) { // Create a queue and enqueue/dequeue strings.
+        Queue<String> q = new LinkedQueue<String>();
+        Scanner in = new Scanner("to be or not to - be - - that - - - is");
+
+        while (in.hasNext()) {
+            String item = in.next();
+            if (!item.equals("-")) {
+                q.enqueue(item);
+            }
+            else if (!q.isEmpty()) {
+                System.out.println(q.dequeue() + " ");
+            }
+        }
+        System.out.println("(" + q.size() + " left on queue)");
+    }
+}
