022d5cb4dadc945b940efe2079103ad1f8ed6301
Sage Bain <bain.sage@student.greenriver.edu>
2024-02-16 13:27:39 -0800

    Created ArrayList test method headers.

---
+++ tests/ArrayListTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListTest {
+
+    @Test
+    void addFront() {
+    }
+
+    @Test
+    void addBack() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void removeFront() {
+    }
+
+    @Test
+    void removeBack() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void size() {
+    }
+}
