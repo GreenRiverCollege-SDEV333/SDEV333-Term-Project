76a188df1d0273f1b84b6c0a4dada2d5e3378330
William Castillo <william.castillo.jr@outlook.com>
2024-02-16 17:29:09 -0800

    Created test class for LinkedList class

---
+++ tests/LinkedListTest.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest
+{
+
+    @Test
+    void addFront()
+    {
+    }
+
+    @Test
+    void addBack()
+    {
+    }
+
+    @Test
+    void add()
+    {
+    }
+
+    @Test
+    void get()
+    {
+    }
+
+    @Test
+    void set()
+    {
+    }
+
+    @Test
+    void removeFront()
+    {
+    }
+
+    @Test
+    void removeBack()
+    {
+    }
+
+    @Test
+    void remove()
+    {
+    }
+
+    @Test
+    void testRemove()
+    {
+    }
+
+    @Test
+    void contains()
+    {
+    }
+
+    @Test
+    void isEmpty()
+    {
+    }
+
+    @Test
+    void size()
+    {
+    }
+
+    @Test
+    void iterator()
+    {
+    }
+}
