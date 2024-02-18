cfc971bfbe01b281775d07bdf6f5ecf677fafab9
Kasyn Tang <ktang520@gmail.com>
2024-02-18 15:59:57 -0800

    Finished LinkedList

---
+++ src/LinkedList.java
@@ -0,0 +1,304 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedList <E> implements List<E>{
+
+    private class Node {
+        E data;
+        Node next;
+    }
+
+    private Node head;
+
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    /**
+     * Add item to the front.
+     *
+     * @param item the item to be added
+     */
+    @Override
+    public void addFront(E item) {
+        Node theNewOne = new Node();
+
+        if (head == null) {
+            head = theNewOne;
+            size++;
+
+        } else {
+            theNewOne.next = head;
+            head = theNewOne;
+
+        }
+    }
+
+    /**
+     * Add item to the back.
+     *
+     * @param item the item to be added
+     */
+    @Override
+    public void addBack(E item) {
+        Node newNode = new Node();
+        newNode.data = item;
+        newNode.next = null;
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+        size++;
+    }
+
+    /**
+     * Add an item at specified index (position).
+     *
+     * @param index    the index where the item should be added
+     * @param item the item to be added
+     */
+    @Override
+    public void add(int index, E item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index is out of range");
+        }
+
+        Node newNode = new Node();
+
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+
+        size++;
+
+    }
+
+    /**
+     * Get the item at a specified index.
+     *
+     * @param index the index where the item should be retrieved
+     * @return the item located at that index
+     */
+    @Override
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of range");
+        }
+
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    /**
+     * Set (save) an item at a specified index. Previous
+     * item at that index is overwritten.
+     *
+     * @param index    the index where the item should be saved
+     * @param item the item to be saved
+     */
+    @Override
+    public void set(int index, E item) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of range");
+        }
+
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        current.data = item;
+    }
+
+    /**
+     * Remove item at the front of the list.
+     *
+     * @return the item that was removed
+     */
+    @Override
+    public E removeFront() {
+        if (head != null) {
+            head = head.next;
+            size--;
+        }
+        return null;
+    }
+
+    /**
+     * Remove item at the back of the list
+     *
+     * @return the item that was removed
+     */
+    @Override
+    public E removeBack() {
+        if (head != null) {
+            if (size == 1) {
+                head = null;
+            } else {
+                Node current = head;
+                while (current.next.next != null) {
+                    current = current.next;
+                }
+                current.next = null;
+            }
+            size--;
+        }
+
+        return null;
+    }
+
+    /**
+     * Remove item from the list
+     *
+     * @param item the item to be removed
+     */
+    @Override
+    public void remove(E item) {
+        Node currentNode = head;
+
+        for (int i = 0; i < size; i++) {
+            if (currentNode.data == item) {
+                currentNode.data = null;
+            }
+            currentNode = currentNode.next;
+        }
+    }
+
+    /**
+     * Remove item at a specified index.
+     *
+     * @param index the index where the item should be removed
+     * @return the item that was removed
+     */
+    @Override
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of range");
+        }
+
+        if (index == 0) return removeFront();
+
+        Node current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+
+        Node temp = current.next;
+        current.next = current.next.next;
+        size--;
+        return temp.data;
+    }
+
+    /**
+     * Checks if an item is in the list.
+     *
+     * @param item the item to search for
+     * @return true if the item is in the list, false otherwise
+     */
+    @Override
+    public boolean contains(E item) {
+        Node current = head;
+        while (current != null) {
+            if (current.data.equals(item)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the list is empty.
+     *
+     * @return true if the list is empty, false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Provides a count of the number of items in the list.
+     *
+     * @return number of items in the list
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new SinglyLinkedIterator();
+    }
+
+    private class SinglyLinkedIterator implements Iterator<E> {
+
+        private Node current;
+
+        public SinglyLinkedIterator() {
+            current = head;
+
+        }
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            if (current == null) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public E next() {
+            if (current == null) {
+                throw new NoSuchElementException("There is no next one to go");
+            }
+
+            E data = current.data;
+            current = current.next;
+
+            return data;
+        }
+    }
+}
