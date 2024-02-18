29af0f1d3890d5d60675d17ba7c2c40fb8d3ebf4
Daniel <knollmdan@gmail.com>
2024-02-17 19:25:16 -0800

    finished linkedqueue and tested with queuetestclient

---
+++ src/LinkedQueue.java
@@ -0,0 +1,110 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedQueue<E> implements Queue<E> {
+    public class Node {
+        Node next;
+        E data;
+    }
+
+    private Node head;
+    private int size;
+
+    /**
+     * Add an item to the queue.
+     *
+     * @param item the item to be added
+     */
+    @Override
+    public void enqueue(E item) {
+        if (size == 0) {
+            head = new Node();
+            head.data = item;
+            head.next = null;
+        } else {
+            Node newNode = new Node();
+            newNode.data = item;
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+
+    }
+
+    /**
+     * Remove an item from the queue.
+     *
+     * @return the item that was removed
+     */
+    @Override
+    public E dequeue() {
+        Node finder = head;
+        while (finder.next != null) {
+            finder = finder.next;
+        }
+        Node finder2 = head;
+        while (finder2.next != finder) {
+            finder2 = finder.next;
+        }
+        finder2.next = null;
+        size--;
+        return finder.data;
+    }
+
+    /**
+     * Checks to see if the queue is empty.
+     *
+     * @return true if the queue is empty, false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return size < 1;
+    }
+
+    /**
+     * Returns a count of the number of items in the queue.
+     *
+     * @return the number of items in the queue
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return (Iterator<E>) new LinkedQueueIterator();
+    }
+    private class LinkedQueueIterator implements Iterator<E> {
+
+        private Node current;
+
+        public LinkedQueueIterator() {
+            current = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (current == null || current.next == null) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        @Override
+        public E next() {
+            if (current == null) {
+                throw new NoSuchElementException("There is no next one to go to!");
+            }
+            E dataValue = (E) current.data;
+            current = current.next;
+            return dataValue;
+        }
+    }
+}
