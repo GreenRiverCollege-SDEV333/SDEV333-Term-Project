3ce236fb9159875fa76a4f7761cbeb0dbe1c3c16
ryderdettloff <rldettloff@gmail.com>
2024-02-18 12:24:03 -0800

    Added other required files..

---
+++ src/LinkedQueue.java
@@ -0,0 +1,54 @@
+import java.util.Iterator;
+
+public class LinkedQueue<E> implements Queue<E>{
+
+    /**
+     * Add an item to the queue.
+     *
+     * @param item the item to be added
+     */
+    @Override
+    public void enqueue(E item) {
+
+    }
+
+    /**
+     * Remove an item from the queue.
+     *
+     * @return the item that was removed
+     */
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    /**
+     * Checks to see if the queue is empty.
+     *
+     * @return true if the queue is empty, false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Returns a count of the number of items in the queue.
+     *
+     * @return the number of items in the queue
+     */
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
