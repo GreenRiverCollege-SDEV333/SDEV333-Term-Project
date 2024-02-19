540c4a82d3d9827a729c513ce933ec73aba69598
kayleyyoung253 <kayley.young253@gmail.com>
2024-02-19 12:43:51 -0800

    Added ArrayList Java class to implement all the methods of the List <E> interface.

---
+++ src/ArrayList.java
@@ -0,0 +1,144 @@
+import java.util.Iterator;
+
+/**
+ *
+ * implement all of the methods in the List <E> interface, and implement the interator()
+ * method from the iterable <E> interface
+ */
+public class ArrayList <E> implements List <E> {
+    /**
+     * Add item to the front.
+     *
+     * @param item the item to be added
+     */
+    @Override
+    public void addFront(E item) {
+
+    }
+
+    /**
+     * Add item to the back.
+     *
+     * @param item the item to be added
+     */
+    @Override
+    public void addBack(E item) {
+
+    }
+
+    /**
+     * Add an item at specified index (position).
+     *
+     * @param i    the index where the item should be added
+     * @param item the item to be added
+     */
+    @Override
+    public void add(int i, E item) {
+
+    }
+
+    /**
+     * Get the item at a specified index.
+     *
+     * @param i the index where the item should be retrieved
+     * @return the item located at that index
+     */
+    @Override
+    public E get(int i) {
+        return null;
+    }
+
+    /**
+     * Set (save) an item at a specified index. Previous
+     * item at that index is overwritten.
+     *
+     * @param i    the index where the item should be saved
+     * @param item the item to be saved
+     */
+    @Override
+    public void set(int i, E item) {
+
+    }
+
+    /**
+     * Remove item at the front of the list.
+     *
+     * @return the item that was removed
+     */
+    @Override
+    public E removeFront() {
+        return null;
+    }
+
+    /**
+     * Remove item at the back of the list
+     *
+     * @return the item that was removed
+     */
+    @Override
+    public E removeBack() {
+        return null;
+    }
+
+    /**
+     * Remove item from the list
+     *
+     * @param item the item to be removed
+     */
+    @Override
+    public void remove(E item) {
+
+    }
+
+    /**
+     * Remove item at a specified index.
+     *
+     * @param i the index where the item should be removed
+     * @return the item that was removed
+     */
+    @Override
+    public E remove(int i) {
+        return null;
+    }
+
+    /**
+     * Checks if an item is in the list.
+     *
+     * @param item the item to search for
+     * @return true if the item is in the list, false otherwise
+     */
+    @Override
+    public boolean contains(E item) {
+        return false;
+    }
+
+    /**
+     * Checks if the list is empty.
+     *
+     * @return true if the list is empty, false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Provides a count of the number of items in the list.
+     *
+     * @return number of items in the list
+     */
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+}
