78a283362616af0ea3c110861f9c17517801c076
Tien Nguyen <nguyen.tien@student.greenriver.edu>
2024-02-18 16:29:13 -0800

    Term Project Part 1 COMPLETED AND READY TO BE GRADED . _________________________________ ArrayList methods, tested, and runtime analysis finished. LinkedList methods, tested, and runtime analysis finished.

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -355,7 +355,14 @@ public class ArrayList <E> implements List <E> {
      */
     @Override
     public boolean isEmpty() {
-        return size != 0;
+        if(size == 0 )
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+
     }
 
     /**
