99722d700b403a1f3f961a250d30cffd403cd27a
Sage Bain <bain.sage@student.greenriver.edu>
2024-02-18 20:44:14 -0800

    Updated to test LinkedStack implementation.

--- src/StackTestClient.java
+++ src/StackTestClient.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class StackTestClient {
     public static void main(String[] args) {
-        Stack<String> s = new ResizingArrayStack<String>();
+        Stack<String> s = new LinkedStack<String>();
         Scanner in = new Scanner("to be or not to - be - - that - - - is");
         while (in.hasNext()) {
         String item = in.next();
