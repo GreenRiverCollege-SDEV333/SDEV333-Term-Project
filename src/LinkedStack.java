e5e3e4d7e782fd3e78e03f0d29c2dbc5165ae752
dhysdrn <dhysdrn@gmail.com>
2024-02-20 10:31:07 -0800

    Modified LinkedQueue, LinkedStack, ResizingArrayStack

--- src/LinkedStack.java
+++ src/LinkedStack.java
@@ -9,6 +9,10 @@ public class LinkedStack<E> implements Stack<E> {
         E item;
         Node next;
     }
+    public LinkedStack(){
+        first = null;
+        size = 0;
+    }
     /**
      * Add an item to the stack.
      *
@@ -104,6 +108,33 @@ public class LinkedStack<E> implements Stack<E> {
             E item = current.item;
             current = current.next;
             return item;
+        }
+
+        /**
+         * Removes from the underlying collection the last element returned
+         * by this iterator (optional operation).  This method can be called
+         * only once per call to {@link #next}.
+         * <p>
+         * The behavior of an iterator is unspecified if the underlying collection
+         * is modified while the iteration is in progress in any way other than by
+         * calling this method, unless an overriding class has specified a
+         * concurrent modification policy.
+         * <p>
+         * The behavior of an iterator is unspecified if this method is called
+         * after a call to the {@link #forEachRemaining forEachRemaining} method.
+         *
+         * @throws UnsupportedOperationException if the {@code remove}
+         *                                       operation is not supported by this iterator
+         * @throws IllegalStateException         if the {@code next} method has not
+         *                                       yet been called, or the {@code remove} method has already
+         *                                       been called after the last call to the {@code next}
+         *                                       method
+         * @implSpec The default implementation throws an instance of
+         * {@link UnsupportedOperationException} and performs no other action.
+         */
+        @Override
+        public void remove() {
+            Iterator.super.remove();
         }
     }
 }
