7bdae28e0d18318c334c792c88f697dd4be7a45d
jEllerGRC <eller.jared@student.greenriver.edu>
2024-02-20 13:42:01 -0800

    Tidied up the code a bit, all done.

--- src/QueueTestClient.java
+++ src/QueueTestClient.java
@@ -1,9 +1,16 @@
+/**
+ *  This class serves as a test client for the methods found in LinkedQueue.java.
+ *  @author Jared Eller
+ *  @verison 1.0
+ *  @date 2/20/24
+ */
+
 import java.util.Scanner;
 
 public class QueueTestClient
 {
     public static void main(String[] args) {
-        Queue<String> q = new LinkedQueue<String>();
+        Queue<String> q = new LinkedQueue<>();
         Scanner in = new Scanner("to be or not to - be - - that - - - is");
         while (in.hasNext())
         {
