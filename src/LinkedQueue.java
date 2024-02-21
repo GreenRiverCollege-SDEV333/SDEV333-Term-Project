7204fec8fecead43bab9a37e36dcd668f18ce645
William Castillo <william.castillo.jr@outlook.com>
2024-02-20 19:43:07 -0800

    Completed methods

--- src/LinkedQueue.java
+++ src/LinkedQueue.java
@@ -2,33 +2,72 @@ import java.util.Iterator;
 
 public class LinkedQueue<E> implements Queue<E>
 {
+    private Node first; // link to least recently added node
+    private Node last;  // link to most recently added node
+    private int size;   // number of items on the queue
+
+    private class Node
+    {   // nested class to define nodes
+        E item;
+        Node next;
+    }
+
     @Override
     public void enqueue(E item)
     {
-
     }
 
     @Override
     public E dequeue()
-    {
-        return null;
+    {   // remove item from the beginning of the list
+        E item = first.item;
+        first = first.next;
+        size--;
+        if (isEmpty())
+        {
+            last = null;
+        }
+        return item;
     }
 
     @Override
     public boolean isEmpty()
     {
-        return false;
+        return first == null;
     }
 
     @Override
     public int size()
     {
-        return 0;
+        return size;
     }
 
     @Override
     public Iterator<E> iterator()
     {
-        return null;
+        return new LinkedQueueIterator();
+    }
+
+    private class LinkedQueueIterator implements Iterator<E>
+    {
+        private Node current = first;
+
+        public boolean hasNext()
+        {
+            return current != null;
+        }
+
+        public void remove()
+        {
+
+        }
+
+        public E next()
+        {
+            E item = current.item;
+            current = current.next;
+            return item;
+        }
+
     }
 }
