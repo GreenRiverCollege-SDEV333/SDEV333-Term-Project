2c1419ce3a7114c5292af69cc372fa0c56a61d33
William Castillo <william.castillo.jr@outlook.com>
2024-02-20 19:10:24 -0800

    Created StackTestClient and main method for ResizingArrayStack class to test

---
+++ src/StackTestClient.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StackTestClient {
+    public static void main(String[] args) {
+        Stack<String> s = new ResizingArrayStack<String>();
+
+        Scanner in = new Scanner("to be or not to - be - - that - - - is");
+
+        while (in.hasNext()) {
+            String item = in.next();
+
+            if (!item.equals("-")) {
+                s.push(item);
+            }
+            else if (!s.isEmpty()) {
+                s.pop();
+                System.out.println("s.pop()" +  " ");
+            }
+        }
+
+        System.out.println("(" + s.size() + " left on the stack)");
+    }
+}
