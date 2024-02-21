d5e4a444b3c789d4c5ef712bb2021619306f4158
William Castillo <william.castillo.jr@outlook.com>
2024-02-20 23:05:15 -0800

    Added JavaDocs

--- src/QueueTestClient.java
+++ src/QueueTestClient.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 
+/**
+ * Tests the LinkedQueue class with a string using a scanner.
+ * Prints the results of the changes in the queue
+ *
+ * @author Will Castillo
+ */
 public class QueueTestClient
 {
+    /**
+     * Tests for the LinkedQueue
+     * @param args Not used
+     */
     public static void main(String[] args)
     {   // create a stack and push/pop strings as direct = on StdIn
         Queue <String> q = new LinkedQueue<>();
