4b90c0b82dc5708259841697585eb4edeb4f63f9
William Castillo <william.castillo.jr@outlook.com>
2024-02-20 23:06:13 -0800

    Added JavaDocs

--- src/StackTestClient.java
+++ src/StackTestClient.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
+/**
+ * Tests the LinkedStack and ResizingArrayStack classes
+ * with a string using a scanner. Prints the results
+ * of the changes in the stacks.
+ *
+ * @author Will Castillo
+ */
 public class StackTestClient {
+
+    /**
+     * Tests for the LinkedQueue
+     * @param args Not used
+     */
     public static void main(String[] args) {
 //        Stack<String> s = new ResizingArrayStack<String>();
         Stack <String> s = new LinkedStack<>();
