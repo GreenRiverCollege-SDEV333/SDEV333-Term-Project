10020645fb9dca22a0f33b977cc290d1249c6eab
William Castillo <william.castillo.jr@outlook.com>
2024-02-20 21:19:42 -0800

    Created test class for LinkedQueue

---
+++ src/QueueTestClient.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class QueueTestClient
+{
+    public static void main(String[] args)
+    {   // create a stack and push/pop strings as direct = on StdIn
+        Queue <String> q = new LinkedQueue<String>();
+
+        Scanner in = new Scanner("to be or not to - be - - that - - - is");
+
+        while (in.hasNext()) {
+            String item = in.next();
+
+            if (!item.equals("-")) {
+                q.enqueue(item);
+            }
+            else if (!q.isEmpty()) {
+                System.out.println(q.dequeue() +  " ");
+            }
+        }
+
+        System.out.println("(" + q.size() + " left on queue)");
+
+    }
+}
