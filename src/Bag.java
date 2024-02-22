43558f2af9ff819417f1196203645def0ae87149
kayleyyoung253 <kayley.young253@gmail.com>
2024-02-22 08:54:54 -0800

    created Bag.java interface file

--- src/Bag.java
+++ src/Bag.java
@@ -3,4 +3,23 @@
  * @param <E> class / data type of the items in the bag
  */
 public interface Bag <E> extends Iterable<E> {
+    /**
+    * Add an item to the bag
+    * @param item the item to be added
+    */
+    void add(E item);
+
+    /**
+     * Checks if the bag is empty or not
+     * @return true if the bag is empty, false otherwise
+     */
+    boolean isEmpty();
+
+    /**
+     * Returns the number of items in the bag
+     * @return the number of items in the bag
+     */
+    int size();
+
+
 }
