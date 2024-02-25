18eadc91dc5aed8bdfe3f0290f8f4b842039094b
ryderdettloff <rldettloff@gmail.com>
2024-02-25 13:19:48 -0800

    added queue methods and logic.

--- src/LinkedQueue.java
+++ src/LinkedQueue.java
@@ -1,6 +1,24 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class LinkedQueue<E> implements Queue<E>{
+    private Node head;
+    private Node tail;
+    private int size;
+
+
+    private class Node
+    {
+        E data;
+        Node nextNode;
+    }
+
+    public LinkedQueue()
+    {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
 
     /**
      * Add an item to the queue.
@@ -8,8 +26,24 @@ public class LinkedQueue<E> implements Queue<E>{
      * @param item the item to be added
      */
     @Override
-    public void enqueue(E item) {
+    public void enqueue(E item)     {
 
+        //create a new head node that will be the top of the queue
+        head = new Node();
+        Node prev = head;
+        head.data = item;
+        //check if queue is empty, if it is set the tail to head(new data)
+        if(isEmpty())
+        {
+            tail = head;
+        }
+        else
+        {
+            //if it isn't empty set the prev node to the head and add item to end of queue
+            prev.nextNode = head;
+        }
+
+        size++;
     }
 
     /**
@@ -18,8 +52,22 @@ public class LinkedQueue<E> implements Queue<E>{
      * @return the item that was removed
      */
     @Override
-    public E dequeue() {
-        return null;
+    public E dequeue()     {
+        if(isEmpty())
+        {
+            //check if stack is empty
+            throw new IllegalStateException("Stack is empty, cannot pop()");
+        }
+        //get tail(back) of queue. if tails next node is empty, set head to null.
+        E item = tail.data;
+        tail = tail.nextNode;
+        if(isEmpty())
+        {
+            head = null;
+        }
+        //decrement size to account for dequeue
+        size--;
+        return item;
     }
 
     /**
@@ -29,7 +77,7 @@ public class LinkedQueue<E> implements Queue<E>{
      */
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     /**
@@ -39,7 +87,7 @@ public class LinkedQueue<E> implements Queue<E>{
      */
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     /**
@@ -47,8 +95,26 @@ public class LinkedQueue<E> implements Queue<E>{
      *
      * @return an Iterator.
      */
-    @Override
     public Iterator<E> iterator() {
-        return null;
+        return new ListIterator();
+    }
+
+    public class ListIterator implements Iterator<E>{
+        // returns ture if iteration has no more elements
+        private Node current = head;
+        public boolean hasNext() {
+            return current != null;
+        }
+        // returns next element in the iteration
+
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            //goes to the next node in the stack and returns it
+            E item = current.data;
+            current = current.nextNode;
+            return item;
+        }
     }
 }
